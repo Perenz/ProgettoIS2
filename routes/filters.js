2538405aac54203c797c57c0585949467161ebc5
Stefano Perenzoni <stefano.perenzoni@unitn.it>
2019-11-30 19:39:03 +0100

    Change directories

--- routes/filters.js
+++ routes/filters.js
@@ -4,16 +4,17 @@ const router = express.Router();
 
 router.get('/:filter', (req, res) => {
     var filter = req.params.filter;
-    console.log("PROVAPROVFA")
+    var source = req.query.source;
+    console.log(source)
     //Call python script
     //Using child_process package
     const spawn = require("child_process").spawn
-    const pythonProcess = spawn('python', ["./routes/blackwhite.py"]);
+    const pythonProcess = spawn('python', ["./routes/scripts/blackwhite.py", source]);
 
     //Listen for data from the python script
     pythonProcess.stdout.on('data', (data) => {
-        res.end(data.toString())
-    })
+        res.end(data.toString());
+    });
 });
 
 module.exports = router;
