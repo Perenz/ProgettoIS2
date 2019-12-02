e0b212f9c3b30957ab86391db6ca63cf56a6a04e
Stefano Perenzoni <stefano.perenzoni@unitn.it>
2019-12-02 11:44:37 +0100

    Add rotate, transpose filters and scripts

--- routes/filters.js
+++ routes/filters.js
@@ -3,16 +3,55 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/:filter', (req, res) => {
-    var filter = req.params.filter;
-    var source = req.query.source;
-    console.log(source)
+    //filter and source are common for every route
+    const filter = req.params.filter;
+    const source = req.query.source;
     //Call python script
     //Using child_process package
     const spawn = require("child_process").spawn
-    const pythonProcess = spawn('python', ["./routes/scripts/blackwhite.py", source]);
+    var pythonProcess;
+    //Call the python script giving all the required params
 
+
+    switch(filter){
+        case "bw":
+            // Params: source
+            pythonProcess = spawn('python', ["./routes/scripts/blackwhite.py", source]);    
+            break;
+        
+        case "transpose":
+            // Params: source, sides,
+            const side = req.query.side;
+            //Check of side in done inside the python script
+            pythonProcess = spawn('python', ["./routes/scripts/transpose.py", source, side]);    
+            break;
+
+        case "rotate":
+            // Params: source, degree
+            const angle = req.query.angle;
+            if(angle){
+                //Check if angle is defined
+                pythonProcess = spawn('python', ["./routes/scripts/rotate.py", source, angle]);
+                break;
+            }
+            else{ 
+                //Error becase angle param can't be recognized
+                res.json({"status":"ERROR", "msg":"Can't recognize the parameter angle"});
+                res.end();
+                //Return from this endpoint, the server keep listening for other requests
+                return 
+            }
+        
+        default:
+            res.json({"status":"ERROR", "msg":"Can't recognize the given filter name"});
+            res.end();
+            //Return from this endpoint, the server keep listening for other requests
+            return
+    }
+    
     //Listen for data from the python script
     pythonProcess.stdout.on('data', (data) => {
+        //Return the data as response to the client
         res.end(data.toString());
     });
 });
