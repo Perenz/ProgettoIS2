3ff1cb41fd2a95ab264ba3595339b6f7fbf003df
gekoramy <luka.mosetti@gmail.com>
2019-12-03 14:17:33 +0100

    add debug info

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,37 @@
-const http = require('http');
 const app = require('../app');
-const port = process.env.PORT || 1112;
+const debug = require('debug')('test:server');
+const http = require('http');
+
+const {port} = require('../config');
+
+app.set('port', port);
 
 const server = http.createServer(app);
 
-app.listen(port);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(`Port ${port} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`Port ${port} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    debug(`Listening on port ${server.address().port}`);
+}
