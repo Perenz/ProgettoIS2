6439b2568bbe93b2042ab376aa894a8647d5226b
Paolo Tasin <paolo.tasin17@gmail.com>
2019-12-11 20:31:40 +0100

    Update index.js

--- routes/video-transformation/index.js
+++ routes/video-transformation/index.js
@@ -4,10 +4,10 @@ const storage = require('../../middlewares/storage');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', impl.video_transformation);
-router.post('/cut', impl.cut);
-router.post('/speedup', impl.speedup);
-router.post('/resize', impl.resize);
+router.get('/', impl.video_transformation, commons.params, impl.bucket, commons.exists, commons.execute);
+router.post('/cut', impl.cut, commons.params, impl.bucket, commons.exists, commons.execute);
+router.post('/speedup', impl.speedup, commons.params, impl.bucket, commons.exists, commons.execute);
+router.post('/resize', impl.resize , commons.params, impl.bucket, commons.exists, commons.execute);
 
 
 module.exports = router;
