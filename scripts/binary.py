04c764560e6ace6ace9f2ef5cc5b0b078c63c4d5
Stefano Perenzoni <stefano.perenzoni@unitn.it>
2019-12-11 18:39:08 +0100

    Fix bug in binary.py

--- scripts/binary.py
+++ scripts/binary.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 name = sys.argv[1]
-threshold = sys.argv[2]
+threshold = int(sys.argv[2])
 
 im = Image.open(name)
 Image.eval(im.convert('L'), (lambda x: 255 if x > threshold else 0)).save(name, format=im.format)
