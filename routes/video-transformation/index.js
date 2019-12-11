8ad8dc2f2ec326dfe113856d2712858975723bab
Luca Mosetti <luka.mosetti@gmail.com>
2019-12-11 20:41:29 +0100

    Update index.js

--- routes/video-transformation/index.js
+++ routes/video-transformation/index.js
@@ -4,7 +4,7 @@ const storage = require('../../middlewares/storage');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', impl.video_transformation, commons.params, impl.bucket, commons.exists, commons.execute);
+router.get('/', impl.video_transformation);
 router.post('/cut', impl.cut, commons.params, impl.bucket, commons.exists, commons.execute);
 router.post('/speedup', impl.speedup, commons.params, impl.bucket, commons.exists, commons.execute);
 router.post('/resize', impl.resize , commons.params, impl.bucket, commons.exists, commons.execute);
