dabc3ac061d70f8eb564e982da9022b77cc3658f
Paolo Tasin <paolo.tasin17@gmail.com>
2019-12-12 10:53:02 +0100

    Elimina video prima di sovrascriverlo

--- scripts/v_cut.py
+++ scripts/v_cut.py
@@ -1,9 +1,12 @@
 from moviepy.editor import VideoFileClip
 import sys
+import os
 
 def cut(name, start_time, end_time):
     #potremo fare controllo errori su fine ed inizio 
     cutted_video = VideoFileClip(name).subclip(float(start_time), float(end_time))#carico il video
+    if os.path.exists(name):
+        os.remove(name)
     cutted_video.write_videofile(name)#salvo il video, #?il nome è da cambiare?
 
 name = sys.argv[1]
--- scripts/v_speedup.py
+++ scripts/v_speedup.py
@@ -1,9 +1,12 @@
 from moviepy.editor import VideoFileClip
 import sys
+import os
 
 def speedup(name, factor):
     source_video = VideoFileClip(name)#carico il video
     sped_up_video = source_video.speedx(float(factor))#velocizzo il video
+    if os.path.exists(name):
+        os.remove(name)
     sped_up_video.write_videofile(name)#salvo il video, #?il nome è da cambiare?
 
 name = sys.argv[1]
