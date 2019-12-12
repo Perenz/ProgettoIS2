3f660c9f2e075788694e5a7ddd3a06f9adc678b8
gekoramy <luka.mosetti@gmail.com>
2019-12-12 09:22:23 +0100

    remove unused code

--- routes/videos/impl.js
+++ routes/videos/impl.js
@@ -1,8 +1,3 @@
 const upload = require('../../middlewares/storage');
 
-exports.bucket = function (req, res, next) {
-    req.query.bucketName = 'videos';
-    next();
-};
-
 exports.upload = upload.arrayMulter('videos', ['video/mp4']);
