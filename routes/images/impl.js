33033d39035f306b416dcf09d8158d7ce569d6ac
Luca Mosetti <luka.mosetti@gmail.com>
2019-12-12 09:24:54 +0100

    remove unused code

--- routes/images/impl.js
+++ routes/images/impl.js
@@ -1,8 +1,3 @@
 const upload = require('../../middlewares/storage');
 
-exports.bucket = function (req, res, next) {
-    req.query.bucketName = 'images';
-    next();
-};
-
 exports.upload = upload.arrayMulter('images', ['image/jpeg', 'image/png']);
