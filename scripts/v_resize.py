98a1ad70902042e10eff6db1c054a928a03d94e8
Paolo Tasin <paolo.tasin17@gmail.com>
2019-12-12 11:05:44 +0100

    Elimina video prima di sovrascriverlo (#16)

--- scripts/v_resize.py
+++ scripts/v_resize.py
@@ -1,10 +1,13 @@
 from moviepy.editor import VideoFileClip
+import os
 import sys
 
 def resize(name, pixel_x, pixel_y):
     #potremo fare controllo errori su fine ed inizio 
     source_video = VideoFileClip(name)#carico il video
-    resized_video = source_video.resize((pixel_x, pixel_y))#taglio il video
+    resized_video = source_video.resize((float(pixel_x), float(pixel_y)))#taglio il video
+    if os.path.exists(name):
+        os.remove(name)
     resized_video.write_videofile(name)#salvo il video, #?il nome è da cambiare?
 
 name = sys.argv[1]
